49b4e4c162bacfdf554de3019a5c6e6492a45bab
gopika <gopika2003@gmail.com>
2025-05-01 14:34:59 +0530

    first commit

---
+++ import mysql.py
@@ -0,0 +1,6 @@
+from mysql.connector import connection
+conn = connection.MySQLConnection(user = "root",
+                             host = "localhost",
+                             password = "gopika1234")
+print(conn):
+
